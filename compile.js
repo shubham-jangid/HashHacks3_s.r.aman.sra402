fa6a824282105cdfcf67edc2cc176705f618bf80
dhruv-karan <dhruvkaran24@gmail.com>
2018-10-27 02:07:14 +0530

    compiled

---
+++ compile.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const indexPath = path.resolve(__dirname, 'contract', 'HealthCare.sol');
+const source = fs.readFileSync(indexPath, 'utf8');
+// console.log(solc.compile(source, 1));
+
+module.exports = solc.compile(source, 1).contracts[':HealthCare'];
+ 
