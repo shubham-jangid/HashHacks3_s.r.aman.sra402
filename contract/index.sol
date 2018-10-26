d8127e93d0809d6f93694e2c630665beee9cf999
s-r-aman <s.r.aman.sra@gmail.com>
2018-10-26 22:43:34 +0530

    Fixed the bug again ��

--- contract/index.sol
+++ contract/index.sol
@@ -13,10 +13,10 @@ contract HealthCare {
         reports[id].push(Report(data1, data2, data3));
     }
 
+    uint[] data1Array;
+    uint[] data2Array;
+    uint[] data3Array;
     function getReports(uint id) public view returns (uint[], uint[], uint[]) {
-        uint[] data1Array;
-        uint[] data2Array;
-        uint[] data3Array;
         for(uint i = 0; i < 3; i++) {
             data1Array.push(reports[id][i].data1);
             data2Array.push(reports[id][i].data2);
