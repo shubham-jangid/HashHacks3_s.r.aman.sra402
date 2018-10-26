f59d5d8b824fcbadb2f8192d80a480557c2ade3e
s-r-aman <s.r.aman.sra@gmail.com>
2018-10-26 19:22:30 +0530

    Added arrays in the reports section.

--- contract/index.sol
+++ contract/index.sol
@@ -7,9 +7,18 @@ contract HealthCare {
         string data3;
     }
     
-    mapping(uint => Report) public reports;
-    
+    mapping(uint => Report[100]) public reports;
+    uint[3] public numbers;
+
     function createReport(uint id, string data1, string data2, string data3) public {
-        reports[id] = Report(data1, data2, data3); 
+        for(uint i = 0; i < 100; i++) {
+            if(reports[id][i]) == 0) {
+                reports[id][i] = Report(data1, data2, data3);
+            }
+        }
+    }
+
+    function getReports(uint id) public view returns (Report[]) {
+        return reports[id];
     }
 }
