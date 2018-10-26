ce0416b6e7953090c569e2c6e9271632199dbac7
s-r-aman <s.r.aman.sra@gmail.com>
2018-10-26 22:31:44 +0530

    Removed bug.

--- contract/index.sol
+++ contract/index.sol
@@ -2,23 +2,26 @@ pragma solidity ^0.4.2;
 
 contract HealthCare {
     struct Report {
-        string data1;
-        string data2;
-        string data3;
+        uint data1;
+        uint data2;
+        uint data3;
     }
     
-    mapping(uint => Report[100]) public reports;
-    uint[3] public numbers;
+    mapping(uint => Report[]) public reports;
 
-    function createReport(uint id, string data1, string data2, string data3) public {
-        for(uint i = 0; i < 100; i++) {
-            if(reports[id][i]) == 0) {
-                reports[id][i] = Report(data1, data2, data3);
-            }
-        }
+    function createReport(uint id, uint data1, uint data2, uint data3) public {
+        reports[id].push(Report(data1, data2, data3));
     }
 
-    function getReports(uint id) public view returns (Report[]) {
-        return reports[id];
+    function getReports(uint id) public view returns (uint[], uint[], uint[]) {
+        uint[] data1Array;
+        uint[] data2Array;
+        uint[] data3Array;
+        for(uint i = 0; i < 3; i++) {
+            data1Array.push(reports[id][i].data1);
+            data2Array.push(reports[id][i].data2);
+            data3Array.push(reports[id][i].data3);
+        }
+        return (data1Array, data2Array, data3Array);
     }
 }
